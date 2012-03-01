68ecba854f52255fe8b2a59f1cb57e5874735aab
weyewe <w.yunnal@gmail.com>
2012-03-02 01:33:56 +0700

    basic setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Potoyook::Application.config.secret_token = '03b173539078f4e519ea56f7ff7b941753f148edaaddda1b2eccd7002201a22ffa7db6c2536dd1ad137c87dc0e100fcdef5e789fd3640033fa8df904c1a2dd21'
